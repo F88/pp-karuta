57d1ffef33175dabb55feaab3988ad1ae70a9333
F88 <685250+F88@users.noreply.github.com>
2026-01-05 16:15:11 +0900

    feat(deck): update release year recipes to include years 2015-2027 in reverse order

--- src/lib/karuta/deck/deck-recipe-manager.ts
+++ src/lib/karuta/deck/deck-recipe-manager.ts
@@ -255,13 +255,15 @@ export class DeckRecipeManager {
 
   private static rangeRecipes: DeckRecipe[] = generateSequentialDecks(7);
   private static releaseYearRecipes: DeckRecipe[] = generateReleaseYearDecks([
-    2017, 2018, 2019, 2020,
-    //
-    2021, 2022, 2023, 2024, 2025,
+    2027,
     //
     2026,
     //
-    2027,
+    2025, 2024, 2023, 2022, 2021,
+    //
+    2020, 2019, 2018, 2017, 2016,
+    //
+    2015, 2014,
   ]);
 
   /**
