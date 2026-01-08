0b6af72a88c45b4d3346b17d67acb219f7daa3e9
F88 <685250+F88@users.noreply.github.com>
2026-01-08 19:19:05 +0900

    feat: update PWA configuration to include new apple touch icon

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -9,9 +9,13 @@ export default defineConfig({
   },
   preset: {
     ...minimal2023Preset,
+    apple: {
+      sizes: [180],
+      padding: 0,
+    },
   },
   images: [
-    // 'public/ppk26-icon-2048x2048.png',
-    'public/ppk26-icon-for-kids-2048x2048.png',
+    // 'public/icons/ppk26-icon-2048x2048.png',
+    'public/icons/ppk26-icon-for-kids-2048x2048.png',
   ],
 });
